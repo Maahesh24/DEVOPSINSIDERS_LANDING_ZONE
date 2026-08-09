8eb6a8265bf40e8970e6f431391eee84ee956174
Mahesh Potdar <maahesh@gmail.com>
2026-08-09 09:44:06 +0530

    Add security: disable public network access and FinOps tags on storage account

--- azurerm_storage_account/main.tf
+++ azurerm_storage_account/main.tf
@@ -1,10 +1,16 @@
 variable "stg" {}
 
 resource "azurerm_storage_account" "storageaccount" {
-  for_each                 = var.stg
-  name                     = each.value.name
-  resource_group_name      = each.value.resource_group_name
-  location                 = each.value.location
-  account_tier             = each.value.account_tier
-  account_replication_type = each.value.account_replication_type
+  for_each                      = var.stg
+  name                          = each.value.name
+  resource_group_name           = each.value.resource_group_name
+  location                      = each.value.location
+  account_tier                  = each.value.account_tier
+  account_replication_type      = each.value.account_replication_type
+  public_network_access_enabled = false
+
+  tags = {
+    Environment = each.value.environment
+    Service     = each.value.service
+  }
 }
